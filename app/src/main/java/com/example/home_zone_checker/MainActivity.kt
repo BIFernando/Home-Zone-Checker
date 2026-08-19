3af18ea341713ab181bcc6de2c2394eb4307b955
qwerty758333 <irangapipi@gmail.com>
2026-08-19 09:53:34 +0530

    Debugging

--- app/src/main/java/com/example/home_zone_checker/MainActivity.kt
+++ app/src/main/java/com/example/home_zone_checker/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.home_zone_checker
 
-import android.os.Bundle
-import android.location.Location
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
